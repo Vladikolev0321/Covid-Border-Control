7e9ccffe8d408823e2755baaac0a9ac144178337
Vladikolev0321 <vladi.kolev2003@gmail.com>
2021-07-05 12:42:23 +0300

    Added connection to a mysql db and some crud operations

---
+++ backend/src/main/java/com/example/demo2/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.example.demo2;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+//public class HelloWorld {
+//
+////    @Autowired
+////    Student student;
+//
+//    @RequestMapping("/hello")
+//    public String hello(){
+//        return "Hello, SAG!";
+//    }
+//
+//    @RequestMapping("/")
+//    public String index(){
+//        return "Index point";
+//    }
+//}
---
+++ backend/src/main/java/com/example/demo2/student/StudentController.java
@@ -0,0 +1,111 @@
+package com.example.demo2.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class StudentController {
+
+
+    private final StudentService studentService;
+    //// For db
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @RequestMapping("/hello")
+    public List<Student> getStudents(){
+        return studentService.getStudents();
+    }
+
+    // Accessing the db contents
+    @GetMapping("/students")
+    public List<Student> index(){
+        return studentRepository.findAll();
+    }
+
+//        Integer age = Integer.parseInt(body.get("age"));
+    @GetMapping("/students/{id}")
+    public Student show(@PathVariable("id") Long id){
+        // return studentRepository.findOne(id).orElse(null);
+        return studentRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping("/students")
+    @ResponseBody
+    public Student createStudent(@RequestBody Student body){
+//        System.out.println(body);
+//        String name = body.get("name");
+//        Integer age = Integer.parseInt(body.get("age"));
+        return studentRepository.save(body);
+    }
+
+    @PutMapping("/students/{id}")
+    public Student update(@PathVariable("id") long id, @RequestBody Map<String, String> body){
+       // int studentId = Integer.parseInt(id);
+        // getting blog
+        Student student = studentRepository.findById(id).orElse(null);
+        student.setName(body.get("name"));
+        student.setAge(Integer.parseInt(body.get("content")));
+        return studentRepository.save(student);
+    }
+
+    @DeleteMapping("students/remove/{id}")
+    public void delete(@PathVariable("id") long id){
+        studentRepository.deleteById(id);
+    }
+
+
+
+    //
+
+    // Accessing the made student service
+    @RequestMapping(value = "/generate", method = RequestMethod.GET)
+    @ResponseBody
+    public void generateProfile() {
+
+        studentService.addStudent(4L, "added", 34);
+    }
+
+    @RequestMapping(value = "/profile/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public Student getProfileWithId(@PathVariable("id") long id) {
+
+        var students = studentService.getStudents();
+        return students.stream().filter(s -> s.getId() == id).findFirst().orElse(null);
+    }
+
+    @RequestMapping(value = "/getAllUsers", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Student> getAllUsers() {
+
+        return studentService.getStudents();
+    }
+
+    @RequestMapping(value = "/delete/{id}")
+    @ResponseBody
+    public void deleteProfileWithThisId(@PathVariable("id") long id) {
+
+//        var students = studentService.getStudents();
+//        var studentToRemove = students.stream().filter(s -> s.getId() == id).findFirst().orElse(null);
+        studentService.deleteStudent(id);
+    }
+
+    @RequestMapping(value = "/update/{id}")
+   // @ResponseBody
+    public void update(@PathVariable("id") long id) {
+        studentService.updateStudent(id);
+
+    }
+
+
+
+}
