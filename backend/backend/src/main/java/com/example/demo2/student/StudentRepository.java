3250c8c931b42e9d12b1c5c65622f0c16e1f467e
Vic-Dim <vdimitrov5940@gmail.com>
2021-07-06 17:13:48 +0300

    Adding new entry v.2

--- backend/backend/src/main/java/com/example/demo2/student/StudentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo2.student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
