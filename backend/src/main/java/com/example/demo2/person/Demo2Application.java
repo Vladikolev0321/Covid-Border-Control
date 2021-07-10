a47d5cb855472cf7f8502df8dd06ac7dccb6898c
Vladikolev0321 <vladi.kolev2003@gmail.com>
2021-07-10 17:04:07 +0300

    We made the face detection, the face comparing of images using LBPH algorithm for Face Recognition and started making the controller method that processes given image from the border

--- backend/src/main/java/com/example/demo2/person/Demo2Application.java
+++ backend/src/main/java/com/example/demo2/person/Demo2Application.java
@@ -1,7 +1,12 @@
 package com.example.demo2.person;
 
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class Demo2Application {
@@ -9,6 +14,30 @@ public class Demo2Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo2Application.class, args);
+
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        String imgFile = "uploads/stelian_tegav.png";
+        Mat src = Imgcodecs.imread(imgFile);
+
+        String xmlFile = "xml/lbpcascade_frontalface.xml";
+        CascadeClassifier cc = new CascadeClassifier(xmlFile);
+
+        MatOfRect faceDetection = new MatOfRect();
+        cc.detectMultiScale(src, faceDetection);
+        System.out.println(String.format("Detected faces: %d", faceDetection.toArray().length));
+
+        for(Rect rect: faceDetection.toArray()) {
+            Imgproc.rectangle(src, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height) , new Scalar(0, 0, 255), 3);
+        }
+        // check if there is only one face or if it there are no faces
+     //   if(faceDetection.toArray().length != 1){
+            System.out.println("Take another picture");
+     //   }else{
+            Imgcodecs.imwrite("uploads/stelian_tegav_out.png", src);
+            System.out.println("Image Detection Finished");
+
+     //   }
+
     }
 
 
---
+++ backend/src/main/java/com/example/demo2/person/FaceCompare.java
@@ -0,0 +1,73 @@
+package com.example.demo2.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+import java.util.Arrays;
+
+@SpringBootApplication
+public class FaceCompare {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(FaceCompare.class, args);
+
+        String basePicPath = "uploads\\";
+        double compareHist = compare_image(basePicPath + "ivan_tegav.png", basePicPath + "ivan_proba2.png");
+        System.out.println(compareHist);
+        if (compareHist > 0.72) {
+            System.out.println("face matching");
+        } else {
+            System.out.println("Face does not match");
+        }
+    }
+
+    // Initialize the face detector
+    static CascadeClassifier faceDetector;
+
+    static {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        faceDetector = new CascadeClassifier("xml/haarcascade_frontalface_alt.xml");
+    }
+    // grayscale face
+    public static Mat conv_Mat(String img) {
+        Mat image0 = Imgcodecs.imread(img);
+
+        Mat image1 = new Mat();
+        // Grayscale
+        Imgproc.cvtColor(image0, image1, Imgproc.COLOR_BGR2GRAY);
+        // Detecting faces
+        MatOfRect faceDetections = new MatOfRect();
+        faceDetector.detectMultiScale(image1, faceDetections);
+        // range of face images in rect
+        for (Rect rect : faceDetections.toArray()) {
+            Mat face = new Mat(image1, rect);
+            return face;
+        }
+        return null;
+    }
+
+    public static double compare_image(String img_1, String img_2) {
+        Mat face_1 = conv_Mat(img_1);
+        Mat face_2 = conv_Mat(img_2);
+        Mat hist_1 = new Mat();
+        Mat hist_2 = new Mat();
+
+        //Color range
+        MatOfFloat ranges = new MatOfFloat(0f, 256f);
+        //Histogram size, the bigger the match, the more accurate (slower)
+        MatOfInt histSize = new MatOfInt(1000);
+
+        Imgproc.calcHist(Arrays.asList(face_1), new MatOfInt(0), new Mat(), hist_1, histSize, ranges);
+        Imgproc.calcHist(Arrays.asList(face_2), new MatOfInt(0), new Mat(), hist_2, histSize, ranges);
+
+        // CORREL correlation coefficient
+        double res = Imgproc.compareHist(hist_1, hist_2, Imgproc.CV_COMP_CORREL);
+        return res;
+    }
+
+}
