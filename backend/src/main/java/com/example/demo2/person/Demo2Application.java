f4db1f61835b798f4f9c578418aadd0dd9982ec8
Vic-Dim <vdimitrov5940@gmail.com>
2021-07-15 06:34:50 +0000

    Deleted useless file

--- backend/src/main/java/com/example/demo2/person/Demo2Application.java
+++ backend/src/main/java/com/example/demo2/person/Demo2Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo2Application {
-    
+
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         SpringApplication.run(Demo2Application.class, args);
